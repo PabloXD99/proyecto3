4b94b4abd8b0d381d94e4c3227e7c15694b2eff0
MyT25 <marco.antonio.noh1999@gmail.com>
2019-04-03 21:06:50 -0600

    CODIGO DE MATRIZ INVERSA

---
+++ MATRIZ_INVERSA_UNIDAD3/src/MAIN_MATRIZ.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MARCKO1999
+ */
+public class MAIN_MATRIZ {
+
+    public static void main(String[] args) {
+inversa i = new inversa();
+i.setLocationRelativeTo(null);
+i.setVisible(true);
+    }
+}
